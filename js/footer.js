7f072570b553a141eb46060f51f04cffda78710d
KarmaScripter <terry.eppler@gmail.com>
2023-06-06 10:42:01 -0400

    up

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@
  * The footer will not work if you open the file directly rather than on a web server.
  *************************************************************************/
 
-(function () {
-  "use strict";
+(function() {
+    "use strict";
 
-  $(function () {
-    $(".footer-container").load("footer.html");
-  });
+    $(function() {
+        $(".footer-container").load("footer.html");
+    });
 })();
--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,10 @@
  * The menu will not work if you open the file directly rather than on a web server.
  *************************************************************************/
 
-(function () {
-  "use strict";
+(function() {
+    "use strict";
 
-  $(function () {
-    $(".menu-container").load("menu.html");
-  });
+    $(function() {
+        $(".menu-container").load("menu.html");
+    });
 })();
